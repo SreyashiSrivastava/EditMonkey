e6e5cda18fa1fe389491af99ab9bb0f7c11e4990
Shreyashi Srivastava <shreyashisrivastava1996@gmail.com>
2024-04-30 06:52:22 +0530

    First commit adding project

---
+++ main.py
@@ -0,0 +1,71 @@
+from flask import Flask, flash, request, render_template, url_for
+from werkzeug.utils import secure_filename
+import cv2
+import os
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'webp', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.secret_key = 'secret key'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def processImage(filename, operation):
+    img = cv2.imread(f"uploads/{filename}")
+    match operation:
+        case "cgray":
+            imgProcessed = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            newFilename = f"static/{filename}"
+            cv2.imwrite(newFilename, imgProcessed)
+            return newFilename
+        case "cwebp":
+            newFilename = f"static/{filename.split('.')[0]}.webp"
+            cv2.imwrite(newFilename, img)
+            return newFilename
+        case "cjpg":
+            newFilename = f"static/{filename.split('.')[0]}.jpg"
+            cv2.imwrite(newFilename, img)
+            return newFilename
+        case "cpng":
+            newFilename = f"static/{filename.split('.')[0]}.png"
+            cv2.imwrite(newFilename, img)
+            return newFilename
+
+    pass
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/")
+def home():
+    return render_template("/index.html")
+
+@app.route("/about")
+def about():
+    return render_template("/about.html")
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        operation = request.form.get("operation")
+        if 'file' not in request.files:
+            flash('No file part')
+            return "error"
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return "No selected file."
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            new = processImage(filename, operation)
+            flash(f"Your image has been processed ans is available <a href='/{new}' target='_blank'>here<a>!")
+            return render_template('/index.html')
+        
+    return render_template('/index.html')
+
+if (__name__) == ("__main__"):
+    app.run(debug=True)
